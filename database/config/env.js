81c300708b6b75a3695148505348963258d1941b
AshKetchumza <ashleymichaelsanders@gmail.com>
2019-11-06 15:07:23 -0600

    Updated readme

--- database/config/env.js
+++ database/config/env.js
@@ -1,5 +1,21 @@
 const env = {};
 
+env.live = {
+  database: 'live_db',
+  username: 'postgres',
+  password: 'postgres',
+  host: '127.0.0.1',
+  dialect: 'postgres'
+};
+
+env.dev = {
+  database: 'dev_db',
+  username: 'postgres',
+  password: 'postgres',
+  host: '127.0.0.1',
+  dialect: 'postgres'
+};
+
 env.test = {
   database: 'test_db',
   username: 'postgres',
